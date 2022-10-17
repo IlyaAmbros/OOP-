785f8b0f91ad0dfcd17c91a4898d0ae560fd735f
IlyaAmbros <108877476+IlyaAmbros@users.noreply.github.com>
2022-10-17 19:35:52 +0300

    Add files via upload

--- test (4).py
+++ test (4).py
@@ -25,22 +25,14 @@ class Products :
         return self.__id
 
     def setId(self , id ):
-        x = ['A' , 'B' , 'C' , 'D' , 'E' , 'F']
-        y = [10 , 11 , 12 , 13 , 14 , 15]
-
         parts = id.split( "-" , 6 )
-        z = int(parts[6][2]) + int(parts[6][4]) + int(parts[6][5])
-        for i in range(len(x)):
-            if y[i] == z :
-                parts[6] = parts[6][0] + x[i] + parts[6][1] + parts[6][3] + "0"
-
-        result = " "
-        for i in range(5) :
-            result += (parts[i] + "-")
-        final_result = result + parts[6]
-        self.__id = final_result
+        x = hex(int(parts[6]))
+        parts_2 = x.split("x")
+        self.__id =  f'{parts[0]}-{parts[1]}-{parts[2]}-{parts[3]}-{parts[4]}-{parts[5]}-{parts_2[1]}'
+        return self.__id
 
 
 product = Products( "2022-01-10-11-01-01-123456" , "Pasta" , 110 )
 print(product)
+
         
